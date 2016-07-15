6b1aeab8ac0592429706c64b58e51d42ee2da38a
sherlockcoin <sherlockcoin@gmail.com>
2016-07-15 19:18:46 +0530

    Updated

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@ from distutils.core import setup, Extension
 
 navcoin_hash_module = Extension('navcoin_hash',
                                  sources = ['navcoinmodule.c',
-                                            'navcoin.c',
+                                            'navcoin.c',								           
                                             'sha3/blake.c',
-                                            'sha3/bmw.c',
-                                            'sha3/groestl.c',
-                                            'sha3/jh.c',
-                                            'sha3/keccak.c',
-                                            'sha3/skein.c',
-                                            'sha3/cubehash.c',
-                                            'sha3/echo.c',
-                                            'sha3/luffa.c',
-                                            'sha3/simd.c',
-											'sha3/shavite.c'
-											'sha3/fugue.c'
-                                            'sha3/hamsi.c'],
+					    'sha3/bmw.c',
+					    'sha3/groestl.c',
+					    'sha3/jh.c',
+					    'sha3/keccak.c',
+					    'sha3/skein.c',
+					    'sha3/cubehash.c',
+					    'sha3/echo.c',
+					    'sha3/luffa.c',
+					    'sha3/simd.c',
+                                            'sha3/hamsi.c',
+                                            'sha3/hamsi_helper.c',
+                                            'sha3/fugue.c',
+					    'sha3/shavite.c'],
+
                                include_dirs=['.', './sha3'])
 
 setup (name = 'navcoin_hash',
